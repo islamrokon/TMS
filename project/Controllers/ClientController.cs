924692a85ef44d640be10e91b16ca42acba123dc
islamrokon <54747900+islamrokon@users.noreply.github.com>
2022-05-09 20:19:11 +0600

    Mid Project

---
+++ project/Controllers/ClientController.cs
@@ -0,0 +1,37 @@
+﻿using project.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace project.Controllers
+{
+    public class ClientController : Controller
+    {
+        // GET: Client
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Signup()
+        {
+            return View(new Client());
+
+        }
+        [HttpPost]
+        public ActionResult Signup(Client c)
+        {
+            if (ModelState.IsValid)
+            {
+                TMSEntities db = new TMSEntities();
+                db.Clients.Add(c);
+                db.SaveChanges();
+                return RedirectToAction("Login");
+            }
+            return View();
+
+        }
+    }
+}
